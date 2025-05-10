dd1dc1ce67b6e6fc96062566b5ec2cedd4ee3e7d
Muhammadamirul0 <muhammadamirul05.am@gmail.com>
2025-05-10 17:04:42 +0700

    first commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -7,15 +7,15 @@ const Hero = () => {
             <div className="hidden sm:block absolute -top-20 -left-20 w-72 h-72 bg-purple-900 opacity-20 rounded-full blur-3xl"></div>
             <div className="hidden sm:block absolute top-20 right-20 w-72 h-72 bg-purple-600 opacity-20 rounded-full blur-3xl"></div>
             <div className="pt-32 uppercase text-center">
-                <h1 className="text-6xl lg:text-8xl font-bold font-inter">
+                <h1 className="text-5xl lg:text-8xl font-bold font-inter">
                     Muhammad<br />Amirul
                 </h1>
-                <p className="text-xl lg:text-4xl pt-6 font-bold font-cascadiacode">
+                <p className="text-base lg:text-4xl pt-6 font-bold font-cascadiacode">
                     Web Developer & Grapich Desainer
                 </p>
             </div>
 
-            <div className="flex justify-center pt-12">
+            <div className="flex justify-center pt-14">
                 <a href='bio' className="font-cascadiacode sm:text-base lg:text-2xl flex flex-col items-center">
                     Ayo Lihat
                     <div className="animate-bounce mt-2">
