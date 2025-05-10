1c96fc1ae547c28ca24178426b167f5daa79fa0b
Muhammadamirul0 <muhammadamirul05.am@gmail.com>
2025-05-10 17:22:22 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Hero from './hero'
-import Header from './header'
+import Header from './Header'
+import Hero from './Hero'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
